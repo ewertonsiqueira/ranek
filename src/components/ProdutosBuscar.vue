592f92463d59a4fd5a0ab77ee2fbfb83aed72885
ewertonsiqueira <ewertoncostaxx@hotmail.com>
2022-02-09 21:25:53 -0300

    teste

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,7 +1,8 @@
 <template>
   <form>
-      <input name="busca" id="busca" type="text" v-model="busca">
+     <input name="busca" id="busca" type="text" v-model="busca" placeholder="Buscar...">
      <input type="submit" id="lupa" value="Buscar" @click.prevent="buscarProdutos">
+
   </form>
 </template>
 
@@ -20,6 +21,37 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+form {
+    max-width: 600px;
+    margin: 30px auto 60px auto;
+    position: relative;
+}
+#busca{
+    width: 100%;
+    /* box-shadow: 0 4px 12px rgba(30, 60, 90, 0.1); */
+    padding: 20px;
+    border: none;
+    /* border-radius: 4px; */
+    /* transition: all 0.3s; */
+}
+#busca:focus,
+#busca:hover {
+    /* outline: none; */
+    transform: scale(1.1);
+    /* box-shadow: 0 6px 12px rgba(30, 60, 90, 0.2); */
+}
+#lupa {
+    width: 62px;
+    height: 62px;
+    background: url("../assets/search.svg")no-repeat center center;
+    text-indent: -150px;
+    border: none;
+    cursor: pointer;
+    position: absolute;
+    top: 0px;
+    right: 0px;
+    box-shadow: none;
+}
 
 </style>
